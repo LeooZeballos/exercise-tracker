78959ef85ded70f1c58e81840708e8261a2f22af
LeooZeballos <zeballosleonel3@gmail.com>
2023-01-24 14:57:07 -0300

    create user

--- frontend/src/components/CreateUser/CreateUser.jsx
+++ frontend/src/components/CreateUser/CreateUser.jsx
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import "./CreateUser.css";
 
 const CreateUser = () => {
+  const [username, setUsername] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const user = {
+      username: username,
+    };
+
+    console.log(user);
+
+    setUsername("");
+  };
+
   return (
     <div>
-      <p>You are on the Create User component!</p>
+      <h3>Create New User</h3>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>Username: </label>
+          <input
+            type="text"
+            required
+            className="form-control"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="submit"
+            value="Create User"
+            className="btn btn-primary"
+          />
+        </div>
+      </form>
     </div>
   )
 };
